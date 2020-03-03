82b7aaf6e30972e5dd437b55d9b0c9ad0032d3c8
Lucas Randazzo <lucas@ombustudio.com.ar>
2020-03-03 16:15:45 -0300

    Added DIST to git

---
+++ dist/pwa/precache-manifest.2ff26f352fb3379f24c32247314d1382.js
@@ -0,0 +1,434 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3872ada54c29885f8914",
+    "url": "css/0baf3333.b8f3bba4.css"
+  },
+  {
+    "revision": "5b6182e17588a116aff9",
+    "url": "css/12e33aa5.1877f178.css"
+  },
+  {
+    "revision": "a8187fbfcb73039401e5",
+    "url": "css/1349159e.881c1c24.css"
+  },
+  {
+    "revision": "efe0946705c3ea814a9d",
+    "url": "css/1a0289c4.5fe8421b.css"
+  },
+  {
+    "revision": "d4c24d4e0c6e299968c7",
+    "url": "css/1ebabaea.24634a50.css"
+  },
+  {
+    "revision": "25b23ee746e6e983f9b8",
+    "url": "css/24e93fe6.6cfde67c.css"
+  },
+  {
+    "revision": "af9fae9a85501f30a7a8",
+    "url": "css/358d3d6d.e0e2ad64.css"
+  },
+  {
+    "revision": "3856d425487dde044e11",
+    "url": "css/4af9dd8f.35d11d1b.css"
+  },
+  {
+    "revision": "a94dbc8b1c8771d2f8eb",
+    "url": "css/555364ff.824eb636.css"
+  },
+  {
+    "revision": "7d279b2316ee9648f73b",
+    "url": "css/6471e29d.b2cff037.css"
+  },
+  {
+    "revision": "95a1bb14ce0c5cb95ad3",
+    "url": "css/app.e24795ea.css"
+  },
+  {
+    "revision": "c3657ca204704b8cc992",
+    "url": "css/bbb26252.c311d1e9.css"
+  },
+  {
+    "revision": "dff791f159969e876e2a",
+    "url": "css/bcd3a11e.0beaad36.css"
+  },
+  {
+    "revision": "971236ed85289027dc23",
+    "url": "css/c197cdf8.a2bdf3b2.css"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.5cb7edfc.woff"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.87284894.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.b00849e0.woff"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.adcde98f.woff"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.bb1e4dc6.woff"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.60fa3c06.woff"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.0509ab09.woff2"
+  },
+  {
+    "revision": "84a9d8e66272a100a4a56841f5de371d",
+    "url": "img/header-bg.84a9d8e6.jpg"
+  },
+  {
+    "revision": "67ca43a9a18cf2020a55138733ae4537",
+    "url": "img/sakura-logo.67ca43a9.png"
+  },
+  {
+    "revision": "8253b2041b913b9c2d41b81342e047da",
+    "url": "index.html"
+  },
+  {
+    "revision": "3872ada54c29885f8914",
+    "url": "js/0baf3333.ac8840a1.js"
+  },
+  {
+    "revision": "5b6182e17588a116aff9",
+    "url": "js/12e33aa5.d4fb1766.js"
+  },
+  {
+    "revision": "a8187fbfcb73039401e5",
+    "url": "js/1349159e.2e309513.js"
+  },
+  {
+    "revision": "1d861e320d9b69b92791",
+    "url": "js/1740c68c.5760a000.js"
+  },
+  {
+    "revision": "efe0946705c3ea814a9d",
+    "url": "js/1a0289c4.69a73e4f.js"
+  },
+  {
+    "revision": "d4c24d4e0c6e299968c7",
+    "url": "js/1ebabaea.40d20448.js"
+  },
+  {
+    "revision": "25b23ee746e6e983f9b8",
+    "url": "js/24e93fe6.7a84e22f.js"
+  },
+  {
+    "revision": "af9fae9a85501f30a7a8",
+    "url": "js/358d3d6d.6a920e12.js"
+  },
+  {
+    "revision": "3856d425487dde044e11",
+    "url": "js/4af9dd8f.eca19943.js"
+  },
+  {
+    "revision": "9e50eb1a215a82f127a8",
+    "url": "js/4b47640d.19e50787.js"
+  },
+  {
+    "revision": "a94dbc8b1c8771d2f8eb",
+    "url": "js/555364ff.75fdd33f.js"
+  },
+  {
+    "revision": "0078b4e0d336e1696b66",
+    "url": "js/63351055.da5e0415.js"
+  },
+  {
+    "revision": "7d279b2316ee9648f73b",
+    "url": "js/6471e29d.4594523b.js"
+  },
+  {
+    "revision": "8dc6c4e698f065a58168",
+    "url": "js/77836302.8802e973.js"
+  },
+  {
+    "revision": "f1c341c89ba0bd5815f9",
+    "url": "js/a56318ec.28c0a425.js"
+  },
+  {
+    "revision": "261f04232b50798b9c7c",
+    "url": "js/a587a016.afc5fb41.js"
+  },
+  {
+    "revision": "329260b404e4fd3ba69a",
+    "url": "js/a589ef70.2fd37710.js"
+  },
+  {
+    "revision": "c11b08182c62ffe27812",
+    "url": "js/a58a2894.45447c29.js"
+  },
+  {
+    "revision": "6a00a05641ed3cd2e4bd",
+    "url": "js/a58d933e.061b52e5.js"
+  },
+  {
+    "revision": "95a1bb14ce0c5cb95ad3",
+    "url": "js/app.6f2d7380.js"
+  },
+  {
+    "revision": "c3657ca204704b8cc992",
+    "url": "js/bbb26252.58767587.js"
+  },
+  {
+    "revision": "dff791f159969e876e2a",
+    "url": "js/bcd3a11e.7578852e.js"
+  },
+  {
+    "revision": "971236ed85289027dc23",
+    "url": "js/c197cdf8.1e24b81e.js"
+  },
+  {
+    "revision": "0c8fbe7724768ba048e4",
+    "url": "js/vendor.f3f5603e.js"
+  },
+  {
+    "revision": "1f736975763ea36c7f8a161d9d1a720b",
+    "url": "js/vendor.f3f5603e.js.LICENSE"
+  },
+  {
+    "revision": "fe829d84265e0a9c690d5c73167afb73",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "4937c4a61985d75bd3cf03ee4034c067",
+    "url": "media/deduction.4937c4a6.mp3"
+  },
+  {
+    "revision": "3798ad2b9fb8d6fb50f5e4aadd885160",
+    "url": "media/noty.3798ad2b.mp3"
+  },
+  {
+    "revision": "53f0723dca354adcd34508343ff4bdbb",
+    "url": "media/open-up.53f0723d.mp3"
+  },
+  {
+    "revision": "805b0a66d63db95a76ddda23e3a8c12f",
+    "url": "statics/app-logo-128x128.png"
+  },
+  {
+    "revision": "f0605401eb2d62da3280bd5a43ca64e4",
+    "url": "statics/autostock-q.css"
+  },
+  {
+    "revision": "ee297b966d7d307e0dd3f5f86b2a43fc",
+    "url": "statics/catalogo-sakura.jpg"
+  },
+  {
+    "revision": "bde75c95c26cbe407d8c3a51149c9047",
+    "url": "statics/icons/apple-icon-120x120.png"
+  },
+  {
+    "revision": "1e6959a9778d40656217fe5eb7e0b7aa",
+    "url": "statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "e776afac4479ee2ad8d6a0397661bf09",
+    "url": "statics/icons/apple-icon-167x167.png"
+  },
+  {
+    "revision": "37ab9045dbdc0edf12ea59b3772e19f9",
+    "url": "statics/icons/apple-icon-180x180.png"
+  },
+  {
+    "revision": "65d8571ec508d854ae4132e6540c476a",
+    "url": "statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "7233d85b329c161211830b1b30fdf0ec",
+    "url": "statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "c7428f8eba0499407710120fea46ead7",
+    "url": "statics/icons/favicon-96x96.png"
+  },
+  {
+    "revision": "04671eaea0c2fc94e31c5ab76ec67d08",
+    "url": "statics/icons/favicon.ico"
+  },
+  {
+    "revision": "805b0a66d63db95a76ddda23e3a8c12f",
+    "url": "statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "3f5de7654b25e5f1640662111d28b490",
+    "url": "statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "15bc9eca3421a7d5a0599a5fb39fe7a8",
+    "url": "statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "f309ab32f7ba3a2d3f50d05ba3d2aaa2",
+    "url": "statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "c8fdfe20437ccd115ee07cb684685ded",
+    "url": "statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "4da6b924d16f06a93f911d7d385d4510",
+    "url": "statics/icons/icon-one.png"
+  },
+  {
+    "revision": "145f3d86b9bae648fa79a9c514692662",
+    "url": "statics/icons/icon-three.png"
+  },
+  {
+    "revision": "10e5081e163bd23f4dcc7278973b7f3f",
+    "url": "statics/icons/icon-two.png"
+  },
+  {
+    "revision": "3acd39ad01fc3d49fc0c74f8d94c343c",
+    "url": "statics/icons/icon_red-arrow-right.png"
+  },
+  {
+    "revision": "529ebf4cee85297995bec01355853a19",
+    "url": "statics/icons/icon_red.png"
+  },
+  {
+    "revision": "6acaf18a3f9f5454be05c204686f9605",
+    "url": "statics/icons/icon_s.png"
+  },
+  {
+    "revision": "e31b9e7ff70bf6ba18ae745a96a0ffe1",
+    "url": "statics/icons/icon_sakura.png"
+  },
+  {
+    "revision": "944fd619fa94a0a2d77666de67d2db10",
+    "url": "statics/icons/icon_stock.png"
+  },
+  {
+    "revision": "5e87d84b6844b3ea92298f9398ebb9fb",
+    "url": "statics/icons/icon_tecnologias.png"
+  },
+  {
+    "revision": "21ef47fc08c7f60fd1f08f3f837f5c43",
+    "url": "statics/icons/icon_white.png"
+  },
+  {
+    "revision": "6624870bb64b1a562e8438e57cdc636a",
+    "url": "statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "ba58d8b825312a31114fb91484dadbeb",
+    "url": "statics/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "561d1be880cf6461b66728294d730061",
+    "url": "statics/icons/type_aceite.png"
+  },
+  {
+    "revision": "2d65e752f5ba515527ac343591907912",
+    "url": "statics/icons/type_aire.png"
+  },
+  {
+    "revision": "41fb465af40a5243a8d9c2c6076c418b",
+    "url": "statics/icons/type_cabina.png"
+  },
+  {
+    "revision": "e49d2a696436a53a379f99043e98b3e4",
+    "url": "statics/icons/type_combustible.png"
+  },
+  {
+    "revision": "8a86b19e42aced1d538639526b5fac2a",
+    "url": "statics/icons/type_kit.png"
+  },
+  {
+    "revision": "6870e229a47c03174fabb33eee4ce427",
+    "url": "statics/product-placeholder.png"
+  },
+  {
+    "revision": "4937c4a61985d75bd3cf03ee4034c067",
+    "url": "statics/sounds/deduction.mp3"
+  },
+  {
+    "revision": "b5e7acadf2f28537c01f00da3611a794",
+    "url": "statics/sounds/eventually.mp3"
+  },
+  {
+    "revision": "3798ad2b9fb8d6fb50f5e4aadd885160",
+    "url": "statics/sounds/noty.mp3"
+  },
+  {
+    "revision": "53f0723dca354adcd34508343ff4bdbb",
+    "url": "statics/sounds/open-up.mp3"
+  },
+  {
+    "revision": "4067a61c7ed086a837b2974b2363bed7",
+    "url": "statics/sounds/point-blank.mp3"
+  },
+  {
+    "revision": "bad356379589902cbf657319115b6d06",
+    "url": "statics/stc/alergia.png"
+  },
+  {
+    "revision": "793714ecbd52402b192a4835e1f28db7",
+    "url": "statics/stc/as-logo.jpg"
+  },
+  {
+    "revision": "4f6a0676f71d1c4783db95c2cfee4a85",
+    "url": "statics/stc/bacteria.png"
+  },
+  {
+    "revision": "9fbebd268045f035b83b0872462e80fd",
+    "url": "statics/stc/bioguard.jpg"
+  },
+  {
+    "revision": "1da307606f7086c8a4f17280d4c55ac8",
+    "url": "statics/stc/filtro-de-aceite-sakura-supreme-guard.jpg"
+  },
+  {
+    "revision": "3975ece134643b71006f4dfa79aa74ae",
+    "url": "statics/stc/filtro-de-aceite-supreme-guard-detalles-tecnicos.jpg"
+  },
+  {
+    "revision": "c678745a5ffdbb2c2bd3c9ef2edff33b",
+    "url": "statics/stc/filtros-de-aire-triple-capa.jpg"
+  },
+  {
+    "revision": "c0b73a2ed81a9d88470d287fe4cbc83a",
+    "url": "statics/stc/micron.png"
+  },
+  {
+    "revision": "7a09be6cf4b7b7ab4c3f77f2e8017036",
+    "url": "statics/stc/odour.png"
+  },
+  {
+    "revision": "582f607626ac697a596b298f173ebe19",
+    "url": "statics/stc/pruebas-de-laboratorio-sakura.png"
+  },
+  {
+    "revision": "ee297b966d7d307e0dd3f5f86b2a43fc",
+    "url": "statics/stc/sakura-filter.jpg"
+  },
+  {
+    "revision": "5dd6380ee635b31190c40b3ab48ce3f4",
+    "url": "statics/stc/sakura.png"
+  },
+  {
+    "revision": "6153d03dd03e7b3391f0ebe40a9674d0",
+    "url": "statics/stc/supreme-guard.jpg"
+  },
+  {
+    "revision": "b0f498d0aa96b23e34673aa53a60d596",
+    "url": "statics/stc/tiempo.png"
+  },
+  {
+    "revision": "1e5991e5af6ce67bd51587999234e4e9",
+    "url": "statics/stc/virus.png"
+  }
+]);
